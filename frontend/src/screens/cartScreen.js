e6f8e4d06155c0a6efa04ec0921d376ce75d3ca5
kanishka4 <kanishka0632@gmail.com>
2022-03-09 21:13:25 +0530

    ui added

--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -1,8 +1,25 @@
 import './cartScreen.css';
+import CartItem from '../components/CartItem';
 
 const cartScreen = () => {
   return (
-    <div className='cartScreen'>cartScreen</div>
+    <div className='cartScreen'>
+      <div className='cartScreenLeft'>
+          <h2>
+            Shopping Cart
+            <CartItem/>
+          </h2>
+      </div>
+      <div className='cartScreenRight'>
+        <div className='cartScreenInfo'>
+        <p> Subtotal (0) items </p>
+        <p>$499</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+        </div>
+      </div>
   )
 }
 
--- frontend/src/screens/productScreen.js
+++ frontend/src/screens/productScreen.js
@@ -2,7 +2,42 @@ import './productScreen.css';
 
 const productScreen = () => {
   return (
-    <div className='productScreen'> productScreen</div>
+    <div className='productScreen'> 
+    
+    <div className='productScreenLeft'>
+      <div className="leftImage">
+        <img src= "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="image">
+          </img>
+      </div>
+      <div className='leftInfo'>
+        <p className='leftName'>Product 1</p>
+        <p>Price : $499</p>
+        <p>Lorem ipsum</p>
+        </div>
+    </div>
+    <div className='productScreenRight'>
+      <div className="rightInfo">
+        <p>
+          Price:<span>$499</span>
+        </p>
+        <p>
+          Status <span>In Stock</span>
+        </p>
+        <p>Qty:
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        </p>
+        <p>
+          <button type="button">Add To Cart</button>
+        </p>
+
+      </div>
+      </div>
+    </div>
   )
 }
 
